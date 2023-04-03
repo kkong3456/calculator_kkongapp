d55329da578894ac59b4ec9dcadbb52b43888bfc
kkong7 <kkongk@naver.com>
2023-04-03 21:40:00 +0900

    answer_text widget end

---
+++ lib/models/history_item.dart
@@ -0,0 +1,24 @@
+class HistoryItem {
+  HistoryItem({
+    required this.operation,
+    required this.result,
+  });
+
+  final String operation;
+  final String result;
+
+  Map get toMap => {
+        'operation': operation,
+        'result': result,
+      };
+
+  static HistoryItem fromMap(Map historyItemAsMap) => HistoryItem(
+      operation: historyItemAsMap['operation'],
+      result: historyItemAsMap['result']);
+
+  @override
+  String toString() {
+    // return super.toString();
+    return 'HistoryItem(operation:$operation,result:$result)';
+  }
+}
